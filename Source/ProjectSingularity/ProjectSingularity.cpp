35ac3941c446d5d5082923f0c34b76f21f724621
Josephine Esposito <josephine.esposito.2000@gmail.com>
2026-02-03 11:00:09 +0100

    init commit
    * initial commit of the repository
    * version used: 5.7.2
    * templated used: C++, Scalable, Blank template

---
+++ Source/ProjectSingularity/ProjectSingularity.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectSingularity.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectSingularity, "ProjectSingularity" );
