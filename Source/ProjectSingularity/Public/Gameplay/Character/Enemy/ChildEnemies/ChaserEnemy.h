c267c8efa95a80a0e7047d70e07d7241743a632c
Lucia Fernandez <luciaafs99@gmail.com>
2026-03-11 14:17:16 +0100

    Create child class enemy

---
+++ Source/ProjectSingularity/Public/Gameplay/Character/Enemy/ChildEnemies/ChaserEnemy.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseEnemy.h"
+#include "ChaserEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTSINGULARITY_API AChaserEnemy : public ABaseEnemy
+{
+	GENERATED_BODY()
+	
+};
