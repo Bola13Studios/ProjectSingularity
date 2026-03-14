aa31aca96990f0b5a9d2f6ee13913f769a3eebcc
Josephine Esposito <josephine.esposito.2000@gmail.com>
2026-03-14 04:04:51 +0100

    removed the hype boxes in L_Default

--- Source/ProjectSingularity/Public/Data/DataAsset/PlayerConfigDataAsset.h
+++ Source/ProjectSingularity/Public/Data/DataAsset/PlayerConfigDataAsset.h
@@ -16,16 +16,28 @@ class PROJECTSINGULARITY_API UPlayerConfigDataAsset : public UDataAsset
 
 public:
 
-	UPROPERTY(EditAnywhere, Category = "Movement", meta = (DisplayName = "Max Walk Speed"))
+	UPROPERTY(EditAnywhere, Category = "Movement", meta = (DisplayName = "Max Walk Speed", ClampMin = "0"))
 	float maxWalkSpeed;
 
-	UPROPERTY(EditAnywhere, Category = "Movement", meta = (DisplayName = "Jump Z Velocity"))
+	UPROPERTY(EditAnywhere, Category = "Movement", meta = (DisplayName = "Jump Z Velocity", ClampMin = "0"))
 	float jumpZVelocity;
 
 	//When falling, amount of lateral movement control available to the character. 0 = no control, 1 = full control at MaxWalkSpeed
 	UPROPERTY(EditAnywhere, Category = "Movement", meta = (DisplayName = "Air Control", ClampMin = "0", ClampMax = "1"))
 	float airControl;
 
-	UPROPERTY(EditAnywhere, Category = "Movement", meta = (DisplayName = "Gravity Scale"))
+	UPROPERTY(EditAnywhere, Category = "Movement", meta = (DisplayName = "Gravity Scale", ClampMin = "0"))
 	float gravityScale;
+
+	UPROPERTY(EditAnywhere, Category = "Movement", meta = (DisplayName = "Ground Friction", ClampMin = "0"))
+	float groundFriction;
+
+	UPROPERTY(EditAnywhere, Category = "Movement|Dash", meta = (DisplayName = "Dash Distance", ClampMin = "0.1"))
+	float dashDistance;
+
+	UPROPERTY(EditAnywhere, Category = "Movement|Dash", meta = (DisplayName = "Dash Time", ClampMin = "0.1"))
+	float dashTime;
+
+	UPROPERTY(EditAnywhere, Category = "Movement|Dash", meta = (DisplayName = "Dash Cooldown", ClampMin = "0.1"))
+	float dashCooldown;
 };
