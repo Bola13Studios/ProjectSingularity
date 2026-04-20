97f72e25fb3c2e83e44d2fd9b77fca903087214c
josephineesposito <josephine.esposito.2000@gmail.com>
2026-04-20 04:56:39 +0200

    readded values to the bp
    * added doc to the methds

--- Source/ProjectSingularity/Public/Components/Interactive/InteractiveStation.h
+++ Source/ProjectSingularity/Public/Components/Interactive/InteractiveStation.h
@@ -21,13 +21,22 @@ class PROJECTSINGULARITY_API UInteractiveStation : public UBaseInteractiveCompon
   GENERATED_BODY()
 
 public:
+  /**
+   * @brief Represents the amount of hype it costs
+   */
   UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bola 13|Data",
             meta = (DisplayName = "Price", ClampMin = 1))
   float hypeCost;
 
+  /**
+   * @brief Represents the amount to give
+   */
   UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bola 13|Data", meta = (DisplayName = "Amount", ClampMin = 1))
   float amount;
 
+  /**
+   * @brief Sets the type to sell
+   */
   UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Bola 13|Data", meta = (DisplayName = "Station Mode"))
   EStationStates m_stationMode;
 
@@ -61,11 +70,35 @@ public:
                                     UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) override;
 
 private:
+  /**
+   * @brief Will change the player's heath
+   * @param _amount 
+   * @param _cost 
+   * @return 
+   */
   bool ChangeHealth(const float& _amount, const float& _cost);
 
+  /**
+   * @brief Will change the amount of stored ammo
+   * @param _amount 
+   * @param _cost 
+   * @return 
+   */
   bool ChangeAmmo(const float& _amount, const float& _cost);
 
+  /**
+   * @brief Will change the max amount of health
+   * @param _amount 
+   * @param _cost 
+   * @return 
+   */
   bool ChangeMaxHealth(const float& _amount, const float& _cost);
 
+  /**
+   * @brief Will change the amount of damage for both weapons
+   * @param _amount 
+   * @param _cost 
+   * @return 
+   */
   bool ChangeDamage(const float& _amount, const float& _cost);
 };
